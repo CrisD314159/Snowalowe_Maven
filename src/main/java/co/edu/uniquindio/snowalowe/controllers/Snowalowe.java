7e19399d2d09df26533732a7bb8a95f6dc4033fe
JUL14Nzz <juliana.hoyosg@uqvirtual.edu.co>
2023-04-02 19:32:07 -0500

    Cambios en el Style.css

--- src/main/java/co/edu/uniquindio/snowalowe/controllers/Snowalowe.java
+++ src/main/java/co/edu/uniquindio/snowalowe/controllers/Snowalowe.java
@@ -4,7 +4,7 @@ public class Snowalowe {
 
     private static Snowalowe snowalowe; //creamos una instancia, esta de momento es = null
 
-    private Snowalowe(){  }    //creamos el constructor de la clase privado para que no se puede acceder a el
+    private Snowalowe(){  }    //creamos el constructor de la clase privado para que no se puede acceder a él
 /*-----------------------------------------------------------------------------------------*/
     // el Synchronized sirve por si estamos usando hilos no creen una instancia diferente sino que los 2 hilos esperen a la finalizacion del metodo
     public synchronized static Snowalowe getSnowalowe() {
